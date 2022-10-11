b5f8584f5ef084552b754205cc0eb732ad584303
evan kirkiles <kirkilese@gmail.com>
2022-10-11 00:23:45 -0400

    Added space page.

--- src/components/SpaceCard/SpaceCard.tsx
+++ src/components/SpaceCard/SpaceCard.tsx
@@ -8,6 +8,7 @@ import * as APIt from '../../supabase/types';
 import s from './SpaceCard.module.scss';
 import Image from 'next/future/image';
 import DOOR from '../../../public/test/door.png';
+import Link from 'next/link';
 
 type SpaceCardProps = {
   space: APIt.Space;
@@ -19,17 +20,24 @@ const SpaceCard: React.FC<SpaceCardProps> = function SpaceCard({ space }) {
       <div className={s.img_preview}>
         <Image src={DOOR} className={s.door_image} alt={'a door'} />
       </div>
-      <div className={s.shadow_overlay}></div>
-      <div className={s.img_container}>
-        <Image src={DOOR} className={s.door_image} alt={'a door'} />
-        <div className={s.meta_info}>
-          {space.location ? (
-            <div className={s.location}>{space.location}</div>
-          ) : null}
-          <div className={s.title}>{space.title}</div>
-          {space.author ? <div className={s.author}>{space.author}</div> : null}
+      <Link href={`/spaces/${space.id}`}>
+        <div className={s.shadow_overlay}></div>
+      </Link>
+      <Link href={`/spaces/${space.id}`}>
+        <div className={s.img_container}>
+          <Image src={DOOR} className={s.door_image} alt={'a door'} />
+          <div className={s.meta_info}>
+            {space.location ? (
+              <div className={s.location}>{space.location}</div>
+            ) : null}
+            <div className={s.title}>{space.title}</div>
+            {space.author ? (
+              <div className={s.author}>{space.author}</div>
+            ) : null}
+          </div>
         </div>
-      </div>
+      </Link>
+
       <div className={s.created_at}>
         {new Date(space.created_at).toDateString()}
       </div>
--- src/pages/spaces/[spaceid].tsx
+++ src/pages/spaces/[spaceid].tsx
@@ -5,30 +5,33 @@
  * 2022 the nobot space,
  */
 
-import { NextPage } from 'next';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
-import { useQuery } from 'react-query';
+import { dehydrate, QueryClient, useQuery } from 'react-query';
 import { getSpace, spaceKeys } from '../../supabase/api/spaces';
+import s from '../../styles/Space.module.scss';
+import Logo from '../../components/Logo/Logo';
+import Link from 'next/link';
 
 /* -------------------------------------------------------------------------- */
 /*                                   TYPING                                   */
 /* -------------------------------------------------------------------------- */
 
-interface SpagePageProps {
+interface SpacePageProps {
   spaceid: string;
 }
 interface QParams extends ParsedUrlQuery {
-  pid?: string;
+  spaceid?: string;
 }
 
 /* -------------------------------------------------------------------------- */
 /*                                  COMPONENT                                 */
 /* -------------------------------------------------------------------------- */
 
-const SpacePage: NextPage<SpagePageProps> = function SpacePage({ spaceid }) {
+const SpacePage: NextPage<SpacePageProps> = function SpacePage({ spaceid }) {
   // use a fallback loading indicator
   const router = useRouter();
   // get the cached space query. we will also re-get the papercraft likes
@@ -54,8 +57,75 @@ const SpacePage: NextPage<SpagePageProps> = function SpacePage({ spaceid }) {
           }}
         />
       </Head>
+      <div className={s.container}>
+        <div className={s.overlay}>
+          <Link href="/">
+            <div className={s.title}>
+              A BIT OF
+              <br />
+              PERSONAL
+              <br />
+              SPACE
+              <div className={s.return_button}>← return</div>
+            </div>
+          </Link>
+          <div className={s.logo}>
+            <Logo />
+          </div>
+          <div className={s.credits}>
+            SPACE OF
+            <br />
+            peter kirkiles
+            <br />
+            winter 2022
+            <br />
+            kent, ct
+          </div>
+          <div className={s.loading_container}>
+            <div>Loading...</div>
+            <div className={s.loading_bar}></div>
+          </div>
+          <div className={s.more_info}>{space.data?.description}</div>
+          <div className={s.instructions}>HI2</div>
+        </div>
+      </div>
     </>
   );
+};
+
+/* -------------------------------------------------------------------------- */
+/*                                     SSG                                    */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Generate papercraft pages for all papercrafts,
+ * @returns
+ */
+export const getStaticPaths: GetStaticPaths = async () => {
+  return { paths: [], fallback: true };
+};
+
+/**
+ * Run the intiial papercraft query on the server. This only queries for public
+ * papercrafts, not worrying about RLS.
+ * @param context
+ * @returns
+ */
+export const getStaticProps: GetStaticProps<SpacePageProps, QParams> = async ({
+  params,
+}) => {
+  const queryClient = new QueryClient();
+  const spaceid = params!.spaceid!;
+  await queryClient.prefetchQuery(spaceKeys.get(spaceid), () =>
+    getSpace(spaceid)
+  );
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      spaceid,
+    },
+    revalidate: false,
+  };
 };
 
 export default SpacePage;
